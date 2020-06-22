6678a78737ed73ab5d0076b281d90fb19ab26dc4
crouchr <richard.crouch@vonage.com>
2020-06-22 18:12:13 +0100

    Added Tags

--- br2020-ami/packer/build_br2020_ami.sh
+++ br2020-ami/packer/build_br2020_ami.sh
@@ -1,4 +1,5 @@
-echo "AWS_ACCESS_KEY_ID : $AWS_ACCESS_KEY_ID"
+echo "AWS_ACCESS_KEY_ID     : $AWS_ACCESS_KEY_ID"
+echo "AWS_SECRET_ACCESS_KEY : $AWS_SECRET_ACCESS_KEY"
 
 packer build \
 -machine-readable \
