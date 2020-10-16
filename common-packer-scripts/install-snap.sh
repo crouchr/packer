cfce70681a9b834ef643756c83d7a3449e67282b
crouchr <richard.crouch@vonage.com>
2020-10-16 11:46:37 +0100

    Added Snap package manger to vagrant boxes

---
+++ common-packer-scripts/install-snap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Install Snap package manager
+# https://snapcraft.io/install/termshark/centos
+
+echo "Installing Snap package manager..."
+sudo yum -y install snapd
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
