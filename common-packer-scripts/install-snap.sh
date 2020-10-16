79b56db0182e1d0dd71c74a622bc636113cc6388
crouchr <richard.crouch@vonage.com>
2020-10-16 15:23:12 +0100

    Added snap seeding

--- common-packer-scripts/install-snap.sh
+++ common-packer-scripts/install-snap.sh
@@ -6,3 +6,6 @@ echo "Installing Snap package manager..."
 sudo yum -y install snapd
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
+
+# Wait until seeding has completed
+sudo snap wait system seed.loaded
