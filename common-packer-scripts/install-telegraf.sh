4ae734506ce8552bdb72507f3371dfe862cac6d7
Richard Crouch <richard.crouch@protonmail.com>
2023-10-20 06:58:43 +0100

    Comment out telegraf for the time being

--- common-packer-scripts/install-telegraf.sh
+++ common-packer-scripts/install-telegraf.sh
@@ -3,10 +3,12 @@
 # Put influxdb on another node
 # https://www.digitalocean.com/community/tutorials/how-to-monitor-system-metrics-with-the-tick-stack-on-centos-7
 
+# Not working
+
 echo "Installing telegraf..."
 
-sudo wget --no-check-certificate \
-   http://web.ermin.lan/br2020-packages/influxdata.repo \
-   -O /etc/yum.repos.d/influxdata.repo
-
-sudo yum -y install telegraf
+#sudo wget --no-check-certificate \
+#   http://web.ermin.lan/br2020-packages/influxdata.repo \
+#   -O /etc/yum.repos.d/influxdata.repo
+#
+#sudo yum -y install telegraf
