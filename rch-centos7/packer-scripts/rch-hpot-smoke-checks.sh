e965f32e589a2bab19636aaaadc1af15e7ba03cd
Richard Crouch <richard.crouch@protonmail.com>
2023-10-23 11:14:20 +0100

    Updated smoke checks

--- rch-centos7/packer-scripts/rch-hpot-smoke-checks.sh
+++ rch-centos7/packer-scripts/rch-hpot-smoke-checks.sh
@@ -3,12 +3,13 @@ set -e
 
 echo 'Run smoke checks on critical installed applications...'
 
+gcc --version
+go version
+
 docker --version
+
 chef-client --version
 
 clamscan --version
 sec -version
-
-go version
-
 nmap --version
