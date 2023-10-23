34c8bc85f1c31b49180d64dad100c1b9f645ee7c
Richard Crouch <richard.crouch@protonmail.com>
2023-10-23 11:12:36 +0100

    install Go from yum

--- common-packer-scripts/install-go.sh
+++ common-packer-scripts/install-go.sh
@@ -4,7 +4,9 @@
 
 echo "Installing Go..."
 
-sudo curl https://dl.google.com/go/go1.18.2.linux-amd64.tar.gz --output go1.18.2.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
-export PATH=$PATH:/usr/local/go/bin
-go version
+yum install -y go
+
+#sudo curl https://dl.google.com/go/go1.18.2.linux-amd64.tar.gz --output go1.18.2.linux-amd64.tar.gz
+#sudo tar -C /usr/local -xzf go1.18.2.linux-amd64.tar.gz
+#export PATH=$PATH:/usr/local/go/bin
+
--- rch-centos7/packer-scripts/rch-hpot-smoke-checks.sh
+++ rch-centos7/packer-scripts/rch-hpot-smoke-checks.sh
@@ -1,12 +1,14 @@
 #/bin/bash
 set -e
 
+echo 'Run smoke checks on critical installed applications...'
+
 docker --version
 chef-client --version
 
 clamscan --version
 sec -version
+
+go version
+
 nmap --version
-
-go --version
-
