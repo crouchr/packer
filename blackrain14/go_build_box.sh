eccc914a44cc84639b51bad928cceab54646f2ef
Richard Crouch <richard.crouch@mail.com>
2023-11-14 12:59:54 +0000

    Removed -debug from Packer build

--- blackrain14/go_build_box.sh
+++ blackrain14/go_build_box.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 echo 'Entered go_build_box.sh'
-#packer build blackrain14.json
-packer build -debug blackrain14.json
+packer build blackrain14.json
+#packer build -debug blackrain14.json
