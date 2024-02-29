94094ffd93817fcd47c787ba1e9bcd23590ce18a
crouchr <richard.crouch@protonmail.com>
2024-02-29 14:22:12 +0000

    Added commented out commands to chef install

--- common-packer-scripts/install-chef-client.sh
+++ common-packer-scripts/install-chef-client.sh
@@ -4,12 +4,13 @@
 #CHEF_CLIENT_VERSION=16.2.73 - known to work
 CHEF_CLIENT_VERSION=18.3.0
 
-echo 'Running install-chef-client.sh...'
+echo 'Running install-chef-client.sh to install Opscode Chef Client...'
 
 USER=vagrant
 
 # Install latest version of Chef Client - speeds up chef solo provisioning of Box
 sudo curl -L https://www.chef.io/chef/install.sh | sudo bash -s -- -v ${CHEF_CLIENT_VERSION}
+# sudo curl -L https://omnitruck.cinc.sh/install.sh | sudo bash -s -- -v 18
 
 sudo mkdir -p /home/${USER}/.chef
 sudo chown ${USER}:${USER} /home/${USER}/.chef
