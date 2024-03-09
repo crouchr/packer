22785076af3cc3b121860a79cc2d40ab6ad37922
crouchr <richard.crouch@protonmail.com>
2024-03-09 13:11:55 +0000

    Fixing CentOS7 Docker build

--- blackrain14/packer-scripts/shrink_disk.sh
+++ blackrain14/packer-scripts/shrink_disk.sh
@@ -11,14 +11,6 @@ count=`df --sync -kP /boot | tail -n1 | awk -F ' ' '{print $4}'`;
 sudo dd if=/dev/zero of=/boot/zero.fill bs=1024 count=$count || echo "dd exit code $? is suppressed"
 sudo rm /boot/zero.fill;
 
-# Zero Out empty space in swap - not needed in VBox
-#echo "Clean up swap partitions"
-#swappart=`cat /proc/swaps | tail -n1 | awk -F ' ' '{print $1}'`
-#swapoff $swappart;
-#dd if=/dev/zero of=$swappart bs=1M || echo "dd exit code $? is suppressed"
-#mkswap $swappart > /dev/null 2>&1;
-#swapon $swappart;
-
 # Sync to ensure delete completes
 sudo sync
 sudo sync
