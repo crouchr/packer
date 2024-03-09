1f85f3c59e539bca4808f7629fc8926545d968d4
crouchr <richard.crouch@protonmail.com>
2024-03-09 17:35:04 +0000

    Bug fix : install-python

--- common-packer-scripts/install-python-3.11.sh
+++ common-packer-scripts/install-python-3.11.sh
@@ -4,12 +4,12 @@ echo "Installing Python 3.11..."
 
 # Install Python 3.11
 sudo mkdir -p /opt/python3
-wget -P /tmp/ https://www.python.org/ftp/python/3.11.6/Python-3.11.6.tgz
-tar xvf /tmp/Python-3.11.6.tgz -C /opt
+wget -q -P /tmp/ https://www.python.org/ftp/python/3.11.6/Python-3.11.6.tgz
+sudo tar xvf /tmp/Python-3.11.6.tgz -C /opt
 cd /opt/Python-3.11.6
 LDFLAGS="${LDFLAGS} -Wl,-rpath=/usr/local/openssl/lib" ./configure --with-openssl=/usr/local/openssl
-make altinstall
+sudo make altinstall
 # /usr/local/bin/pip3.11 install --upgrade pip
 
 # smoke test
-python3 --version
+# python3 --version
