8f43a4063ac9926c2814becfe9060469498a6640
crouchr <richard.crouch@protonmail.com>
2024-03-09 19:54:34 +0000

    Bug fix : mariadb

--- common-packer-scripts/install-mariadb.sh
+++ common-packer-scripts/install-mariadb.sh
@@ -3,7 +3,7 @@
 
 echo "Installing MariaDB SQL Database System..."
 
-sudo echo -e "[mariadb]\nname=MariaDB Repository\nbaseurl=http://yum.mariadb.org/10.4/centos7-amd64\ngpgcheck=1\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" | tee /etc/yum.repos.d/MariaDB.repo
+sudo echo -e "[mariadb]\nname=MariaDB Repository\nbaseurl=http://yum.mariadb.org/10.4/centos7-amd64\ngpgcheck=1\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" | sudo tee /etc/yum.repos.d/MariaDB.repo
 
 # MariaDB 10.x
 sudo yum -y install MariaDB-server MariaDB-client MariaDB-devel
