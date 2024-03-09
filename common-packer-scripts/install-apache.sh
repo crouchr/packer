c816f7681e9cad54652ef4f067a1ee563fc7084a
crouchr <richard.crouch@protonmail.com>
2024-03-09 20:17:28 +0000

    Bug fix : apache

--- common-packer-scripts/install-apache.sh
+++ common-packer-scripts/install-apache.sh
@@ -3,8 +3,8 @@
 echo "Installing Apache 2.4..."
 
 # Apache, PHP and WAF for web front end
-yum install -y httpd httpd-devel mod_ssl
-yum install -y mod_security
-yum install -y dnsmasq bind-utils
-yum -y install php php-common php-mysql php-pdo php-intl php-gd php-xml php-mbstring
+sudo yum install -y httpd httpd-devel mod_ssl
+sudo yum install -y mod_security
+sudo yum install -y dnsmasq bind-utils
+sudo yum -y install php php-common php-mysql php-pdo php-intl php-gd php-xml php-mbstring
 echo "date.timezone = Europe/London" >> /etc/php.ini
