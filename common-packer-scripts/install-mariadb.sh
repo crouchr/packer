e76fbf0ae2f4f21653558c2d409b877cb9ac381e
crouchr <richard.crouch@protonmail.com>
2024-03-09 19:21:54 +0000

    Bug fix : temp remove install-python.sh

--- common-packer-scripts/install-mariadb.sh
+++ common-packer-scripts/install-mariadb.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eux
 # client, server and C connector
 
-echo -e "[mariadb]\nname=MariaDB Repository\nbaseurl=http://yum.mariadb.org/10.4/centos7-amd64\ngpgcheck=1\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" | tee /etc/yum.repos.d/MariaDB.repo
+echo "Installing MariaDB SQL Database System..."
 
+sudo echo -e "[mariadb]\nname=MariaDB Repository\nbaseurl=http://yum.mariadb.org/10.4/centos7-amd64\ngpgcheck=1\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" | tee /etc/yum.repos.d/MariaDB.repo
 
-# MariaDB 10.4
+# MariaDB 10.x
 sudo yum -y install MariaDB-server MariaDB-client MariaDB-devel
-
