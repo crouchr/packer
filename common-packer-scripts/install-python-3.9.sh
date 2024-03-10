7f2603031f112bff4e1d66af0d70a975a685bcb8
crouchr <richard.crouch100@gmail.com>
2024-03-10 20:12:22 +0000

    Bug fix : python3.9

--- common-packer-scripts/install-python-3.9.sh
+++ common-packer-scripts/install-python-3.9.sh
@@ -9,7 +9,7 @@ sudo mkdir -p /opt/python3
 wget -q -P /tmp/ https://www.python.org/ftp/python/3.9.17/Python-3.9.17.tgz
 sudo tar xvf /tmp/Python-3.9.17.tgz -C /opt
 cd /opt/Python-3.9.17
-LDFLAGS="${LDFLAGS} -Wl,-rpath=/usr/local/openssl/lib" ./configure --with-openssl=/usr/local/openssl --enable-optimizations
+LDFLAGS= "-Wl,-rpath=/usr/local/openssl/lib" ./configure --with-openssl=/usr/local/openssl --enable-optimizations
 sudo make altinstall
 # /usr/local/bin/pip3.11 install --upgrade pip
 
