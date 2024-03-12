cb1e24aa2299873cc6408e8e5bee117ee20efcaa
crouchr <richard.crouch100@gmail.com>
2024-03-12 21:34:30 +0000

    Bug fix : python

--- common-packer-scripts/install-python-3.9.sh
+++ common-packer-scripts/install-python-3.9.sh
@@ -3,17 +3,18 @@
 echo "Installing Python 3.9.17 ..."
 
 # Install Python 3.9.17
-# sudo yum install -y openssl-devel bzip2-devel libffi-devel zlib-devel
 sudo rm -rf /opt/python3
 sudo mkdir -p /opt/python3
 wget -q -P /tmp/ https://www.python.org/ftp/python/3.9.17/Python-3.9.17.tgz
 sudo tar xvf /tmp/Python-3.9.17.tgz -C /opt
 
-#cd /opt/Python-3.9.17
-#LDFLAGS="-Wl,-rpath=/usr/local/openssl/lib" ./configure --with-openssl=/usr/local/openssl --enable-optimizations
-#LDFLAGS="-Wl,-rpath=/usr/lib64/opensslocal/openssl/lib" ./configure --enable-optimizations
-#sudo make altinstall
-# /usr/local/bin/pip3.11 install --upgrade pip
+cd /opt/Python-3.9.17
+LDFLAGS="-Wl,-rpath=/usr/include/openssl" ./configure --enable-optimizations
+sudo make altinstall
 
 # smoke test
-# python3 --version
+python3.9 --version
+
+#LDFLAGS="-Wl,-rpath=/usr/local/openssl/lib" ./configure --with-openssl=/usr/local/openssl --enable-optimizations
+#LDFLAGS="-Wl,-rpath=/usr/lib64/opensslocal/openssl/lib" ./configure --enable-optimizations
+# /usr/local/bin/pip3.11 install --upgrade pip
