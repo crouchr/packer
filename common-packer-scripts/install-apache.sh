3d0ba4cae7d6a610ac0821f6b3b6bb2c6ff7dbbf
crouchr <richard.crouch100@gmail.com>
2024-03-13 12:34:36 +0000

    Bug fix : apache

--- common-packer-scripts/install-apache.sh
+++ common-packer-scripts/install-apache.sh
@@ -5,13 +5,12 @@ echo "Installing Apache 2.4, PHP, ModSSL and ModSecurity..."
 # Apache, PHP and WAF for web front end
 sudo yum install -y httpd httpd-devel mod_ssl
 sudo yum install -y mod_security
-sudo yum install -y dnsmasq bind-utils
+sudo yum install -y dnsmasq bind-utils lynx
 sudo yum -y install php php-common php-mysql php-pdo php-intl php-gd php-xml php-mbstring
 
 # php.ini previously copied into VBOX /tmp dir using file provisioner
 sudo cp /tmp/php.ini /etc/php.ini
 
 echo "Starting httpd..."
-#sudo systemctl enable httpd.service
-#sudo systemctl start httpd.service
-
+sudo systemctl enable httpd.service
+sudo systemctl start httpd.service
