916330b199fe2fbda1a91c1214a4002ab152680d
Richard Crouch <richard.crouch@protonmail.com>
2024-03-19 21:22:50 +0000

    Fix ssl bug in install_apache.sh

--- common-packer-scripts/install-apache.sh
+++ common-packer-scripts/install-apache.sh
@@ -4,7 +4,7 @@ echo "Installing Apache 2.4, PHP, ModSSL and ModSecurity..."
 
 # Apache, PHP and WAF for web front end
 sudo yum install -y httpd httpd-devel mod_ssl
-sudo yum install -y mod_security
+# sudo yum install -y mod_security
 sudo yum install -y dnsmasq bind-utils lynx
 sudo yum -y install php php-common php-mysql php-pdo php-intl php-gd php-xml php-mbstring
 
@@ -12,6 +12,6 @@ sudo yum -y install php php-common php-mysql php-pdo php-intl php-gd php-xml php
 sudo cp /tmp/php.ini /etc/php.ini
 
 # Currently causing a failure in Packer so commented out - only needed for Web-based UI
-# echo "Starting httpd..."
-# sudo systemctl enable httpd.service
-# sudo systemctl start httpd.service
+echo "Starting httpd..."
+sudo systemctl enable httpd.service
+sudo systemctl start httpd.service
