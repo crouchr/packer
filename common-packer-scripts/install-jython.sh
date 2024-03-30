270004a3ac7ca62fcbfde2fd7b964561f57bf52f
Richard Crouch <richard.crouch@protonmail.com>
2024-03-30 19:05:45 +0000

    bug fix : removed Grafana from tradr7

--- common-packer-scripts/install-jython.sh
+++ common-packer-scripts/install-jython.sh
@@ -2,10 +2,7 @@
 
 echo "Installing Jython..."
 
-sudo yum -y install grafana
-sudo mkdir -p /etc/grafana
-
-# jython installer previously copied into VBOX /tmp dir using file provisioner
+# Jython installer previously copied into VBOX /tmp dir using file provisioner
 sudo cp /tmp/jython-installer-2.7.3.jar /tmp/jython-installer-2.7.3.jar
 
 # Do the install here once I know what it is
