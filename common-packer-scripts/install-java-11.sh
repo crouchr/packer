98b849ecff8e1cfe9301421bc6bbaf0db18e2f77
Richard Crouch <richard.crouch@protonmail.com>
2024-03-30 12:44:11 +0000

    Added X Windows and Java 11

--- common-packer-scripts/install-java-11.sh
+++ common-packer-scripts/install-java-11.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo 'Installing Java agent...'
+echo 'Installing Java 11 JDK...'
 sudo yum install -y java-11-openjdk-devel
---
+++ common-packer-scripts/install-x-windows.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+https://www.quora.com/How-can-I-install-X-Windows-on-CentOS-7
+
+echo "Installing X Windows..."
+sudo yum -y groupinstall "X Window System"
