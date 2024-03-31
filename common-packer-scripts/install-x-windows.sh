2170e99592cc831bf113ce27101913f497ef2b1f
crouchr <richard.crouch@protonmail.com>
2024-03-31 13:05:18 +0100

    swapped out Fluxbox from MATE - minimise footprint

--- common-packer-scripts/install-x-windows.sh
+++ common-packer-scripts/install-x-windows.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
+# Minimum install of X to allow running X over ssh
 # https://www.quora.com/How-can-I-install-X-Windows-on-CentOS-7
 
 echo "Installing X Windows..."
 sudo yum -y groupinstall "X Window System"
 sudo yum -y install xorg-x11-apps
 
-echo "Installing MATE desktop..." # what about fluxbox ?
-sudo yum -y groupinstall "MATE Desktop"
+# echo "Installing MATE desktop..."
+# sudo yum -y groupinstall "MATE Desktop"
+
+echo "Installing Fluxbox Window Manager..."
+sudo yum -y install fluxbox
 
 echo "Enable GDM..."
 sudo systemctl enable lightdm
