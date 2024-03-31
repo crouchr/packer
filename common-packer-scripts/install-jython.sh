849774b0a58ca385865186a0154bfd4e87ccf31a
crouchr <richard.crouch@protonmail.com>
2024-03-31 11:27:49 +0100

    bug fixes : Jython installer

--- common-packer-scripts/install-jython.sh
+++ common-packer-scripts/install-jython.sh
@@ -2,9 +2,6 @@
 
 echo "Installing Jython..."
 
-# Jython installer previously copied into VBOX /tmp dir using file provisioner
-# sudo cp /tmp/jython-installer-2.7.3.jar /tmp/jython-installer-2.7.3.jar
-
 # Do the install here once I know what it is
 cd /tmp
 java -jar jython-installer-2.7.3.jar <<END
