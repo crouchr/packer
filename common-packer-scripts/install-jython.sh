588ef1e30278c75b02b1f11c47ee81ffca12d08b
crouchr <richard.crouch@protonmail.com>
2024-03-31 21:08:11 +0100

    bug fix : jython expect script

--- common-packer-scripts/install-jython.sh
+++ common-packer-scripts/install-jython.sh
@@ -4,7 +4,7 @@ echo "Installing Jython..."
 pwd     # This displayed /home/vagrant
 
 # smoke check that java is installed
-# java --version
+java --version
 
 # Use expect to drive the installer
 sudo yum -y install expect
@@ -12,11 +12,11 @@ sudo yum -y install expect
 # Run expect to answer the questions in the interactive installer script
 # see https://www.thegeekstuff.com/2010/10/expect-examples/
 
-sudo cp /tmp/jython-installer-2.7.3.jar /home/vagrant/jython-installer-2.7.3.jar
+sudo cp /tmp/jython-installer.jar /home/vagrant/jython-installer.jar
 sudo cp /tmp/install-jython.exp /home/vagrant/install-jython.exp
 
-# sudo chmod +x /home/vagrant/install-jython.exp
-# sudo ./install-jython.exp
+sudo chmod +x /home/vagrant/install-jython.exp
+sudo ./install-jython.exp
 
 ls -laF /home/vagrant
 
