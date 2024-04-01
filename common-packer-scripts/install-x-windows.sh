8be1ce3babb61307b285c24ab4e49c9c69e2bf21
crouchr <richard.crouch@protonmail.com>
2024-04-01 10:18:00 +0100

    bug fix : fluxbox

--- common-packer-scripts/install-x-windows.sh
+++ common-packer-scripts/install-x-windows.sh
@@ -12,11 +12,6 @@ sudo yum -y install xorg-x11-apps
 echo "Installing Fluxbox Window Manager..."
 sudo yum -y install fluxbox
 
-# Fluxbox
-#echo "Enable GDM..."
-#sudo systemctl enable gdm
-#sudo systemctl start gdm
-
 # MATE
 # echo "Enable LightDM..."
 # sudo systemctl enable lightdm
