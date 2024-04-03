bd951bc2d4002ce5ac996072467da8b62884e4d3
Richard Crouch <richard.crouch@protonmail.com>
2024-04-03 19:39:46 +0100

    bug fix : incorrect web server name

--- common-packer-scripts/install-chef-workstation.sh
+++ common-packer-scripts/install-chef-workstation.sh
@@ -27,7 +27,7 @@ sudo mkdir /var/log/chef
 sudo touch /var/log/chef/client.log
 
 # files too big for repo - store on Slackware 15 web-server
-sudo curl -o /tmp/chef-workstation-24.2.1058-1.el7.x86_64.rpm http://packer.ermin.lan/centos7-packages/chef-workstation-24.2.1058-1.el7.x86_64.rpm
+sudo curl -o /tmp/chef-workstation-24.2.1058-1.el7.x86_64.rpm http://ansible-packer.ermin.lan/centos7-packages/chef-workstation-24.2.1058-1.el7.x86_64.rpm
 
 # files that must not be stored in public repo - store on Slackware 15 web-server
 sudo curl -o /tmp/crouchr.pem http://ansible-packer.ermin.lan/chef-pems/crouchr.pem
