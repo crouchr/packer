c1b952290d7c740c5be60fb1012e8f42ed2fb65b
crouchr <richard.crouch@protonmail.com>
2024-04-04 06:17:09 +0100

    bug fix : incorrect web download from packer node

--- common-packer-scripts/install-chef-workstation.sh
+++ common-packer-scripts/install-chef-workstation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eux
 # Install Chef Workstation - i.e. includes knife which Chef Client does not
 
+# FIXME : hostname of ansible-packer node breaks so have to use IP
+
 # CHEF_CLIENT_VERSION=18.4.12   # use same as VCC
 
 echo 'Running install-chef-workstation.sh to install Opscode Chef WorkstationC...'
@@ -27,17 +29,20 @@ sudo mkdir /var/log/chef
 sudo touch /var/log/chef/client.log
 
 # files too big for repo - store on Slackware 15 web-server
-sudo curl -o /tmp/chef-workstation-24.2.1058-1.el7.x86_64.rpm http://ansible-packer.ermin.lan/centos7-packages/chef-workstation-24.2.1058-1.el7.x86_64.rpm
+sudo curl -o /tmp/chef-workstation-24.2.1058-1.el7.x86_64.rpm http://192.168.1.4/centos7-packages/chef-workstation-24.2.1058-1.el7.x86_64.rpm
 
 # files that must not be stored in public repo - store on Slackware 15 web-server
-sudo curl -o /tmp/crouchr.pem http://ansible-packer.ermin.lan/chef-pems/crouchr.pem
-sudo curl -o /tmp/ermin-validator.pem http://ansible-packer.ermin.lan/chef-pems/ermin-validator.pem
+sudo curl -o /tmp/client.pem http://192.168.1.4/chef-pems/crouchr.pem
+sudo curl -o /tmp/ermin-validator.pem http://192.168.1.4/chef-pems/ermin-validator.pem
+
+cat /tmp/client.pem
+cat /tmp/ermin-validator.pem
 
 # Chef files previously copied into VBOX /tmp dir using file provisioner
 sudo cp /tmp/client.rb /etc/chef/client.rb
 
 # install config files
-sudo cp /tmp/crouchr.pem /etc/chef/crouchr.pem
+sudo cp /tmp/client.pem /etc/chef/client.pem
 sudo cp /tmp/ermin-validator.pem /etc/chef/ermin-validator.pem
 
 # install Chef Workstation
