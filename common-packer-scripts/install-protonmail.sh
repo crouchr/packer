36589026855bf3b8cd8db13c9ed7a2d96e307f4d
crouchr <richard.crouch@protonmail.com>
2024-04-06 20:51:37 +0100

    bug fix : install-protonmail.sh

--- common-packer-scripts/install-protonmail.sh
+++ common-packer-scripts/install-protonmail.sh
@@ -6,8 +6,8 @@ echo "Installing Protonmail Bridge..."
 
 # Run expect to answer the questions in the interactive installer script
 # see https://www.thegeekstuff.com/2010/10/expect-examples/
-wget -O /tmp/pass-1.5.2.el7.noarch.rpm http://192.168.1.4/centos-packages/pass-1.5.2.el7.noarch.rpm
-wget -O /tmp/protonmail-bridge-3.10.0-1.x86_64.rpm http://192.168.1.4/centos-packages/protonmail-bridge-3.10.0-1.x86_64.rpm
+wget -O /tmp/pass-1.5.2.el7.noarch.rpm http://192.168.1.4/centos7-packages/pass-1.5.2.el7.noarch.rpm
+wget -O /tmp/protonmail-bridge-3.10.0-1.x86_64.rpm http://192.168.1.4/centos7-packages/protonmail-bridge-3.10.0-1.x86_64.rpm
 
 yum -y localinstall /tmp/pass-1.5.2.el7.noarch.rpm
 yum -y localinstall /tmp/protonmail-bridge-3.10.0-1.x86_64.rpm
