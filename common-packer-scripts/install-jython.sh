326f44209409859ab3ddf6c741deb35bd692ba29
crouchr <richard.crouch@protonmail.com>
2024-04-10 20:48:13 +0100

    Revert the ossec and jython cookbooks

--- common-packer-scripts/install-jython.sh
+++ common-packer-scripts/install-jython.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
-
-##!/bin/bash -eux FIXME : re-instate when working
+#!/bin/bash -eux
 
 # dependency : expect
 
@@ -13,14 +11,11 @@ java --version
 # Run expect to answer the questions in the interactive installer script
 # see https://www.thegeekstuff.com/2010/10/expect-examples/
 
-# keep installation files 'tidied away'
-sudo mkdir -p /home/vagrant/installer
+sudo cp /tmp/jython-installer.jar /home/vagrant/jython-installer.jar
+sudo cp /tmp/install-jython.exp /home/vagrant/install-jython.exp
 
-sudo cp /tmp/jython-installer.jar /home/vagrant/installer/jython-installer.jar
-sudo cp /tmp/install-jython.exp /home/vagrant/installer/install-jython.exp
-
-sudo chmod +x /home/vagrant/installer/install-jython.exp
-sudo ./home/vagrant/installer/install-jython.exp
+sudo chmod +x /home/vagrant/install-jython.exp
+sudo ./install-jython.exp
 
 ls -laF /home/vagrant
 
@@ -28,3 +23,4 @@ echo "# >> Added during Packer build" >> ${HOME}/.bashrc
 echo  "export PATH=$PATH:/opt/jython-2.7.3/bin" >> ${HOME}/.bashrc
 echo "# << End of block added during Packer build" >> ${HOME}/.bashrc
 echo $PATH
+
