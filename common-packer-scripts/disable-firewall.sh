5b223501149ab078ea24ced4150c1273b545b4fa
crouchr <richard.crouch@protonmail.com>
2024-04-10 13:50:53 +0100

    disable firewall during packer build

--- common-packer-scripts/disable-firewall.sh
+++ common-packer-scripts/disable-firewall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eux
 # Once everything working then re-instate firewall and add kibana and grafana ports opened
+# firewall-cmd --add-port=3306/tcp
+# firewall-cmd --permanent --add-port=3306/tcp
 
 echo 'Running disable-firewall.sh to disable firewall...'
 
