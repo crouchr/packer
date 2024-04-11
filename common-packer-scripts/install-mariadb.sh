e4099fb588254dfe703081454c0338d9d7577dce
crouchr <richard.crouch@protonmail.com>
2024-04-11 08:34:41 +0100

    Explicitly copy client and server configuration files to target machine for MariaDB

--- common-packer-scripts/install-mariadb.sh
+++ common-packer-scripts/install-mariadb.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -eux
 # client, server and C connector
 
-echo "Installing MariaDB SQL Database System..."
+echo "Installing MariaDB SQL Database System v10.4 ..."
 
 sudo echo -e "[mariadb]\nname=MariaDB Repository\nbaseurl=http://yum.mariadb.org/10.4/centos7-amd64\ngpgcheck=1\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" | sudo tee /etc/yum.repos.d/MariaDB.repo
 
 # MariaDB 10.4
 sudo yum -y install MariaDB-server MariaDB-client MariaDB-devel
 
+# Copy configuration files across
+sudo cp /tmp/client.cnf /etc/mycnf.d/client.cnf
+sudo cp /tmp/server.cnf /etc/mycnf.d/server.cnf
+
 echo "Starting MariaDB..."
 sudo systemctl enable mariadb
 sudo systemctl start mariadb
+
+echo "Finished installing MariaDB SQL Database System v10.4"
