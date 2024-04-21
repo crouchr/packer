5c447ce8a01a9cf85feb01c663c85d0a6b497213
crouchr <richard.crouch@protonmail.com>
2024-04-21 18:18:38 +0100

    ossec v3.0.0 but no openssl 1.1.1k install

--- common-packer-scripts/install-ossec-3-local.sh
+++ common-packer-scripts/install-ossec-3-local.sh
@@ -6,9 +6,10 @@
 # https://www.vultr.com/docs/how-to-install-ossec-hids-on-a-centos-7-server/
 # https://linuxadmin.io/ossec-install-centos-7/
 
-echo 'Installing OSSEC (local) v3.7.0...'
+# echo 'Installing OSSEC (local) v3.7.0...'
+echo 'Installing OSSEC (local) v3.0.0...'
 
-sudo yum install -y inotify bind-utils pcre2-devel
+sudo yum install -y inotify bind-utils pcre2-devel zlib-devel libevent-devel systemd-devel
 
 # keep installation files 'tidied away'
 sudo mkdir -p /home/vagrant/installer
@@ -16,10 +17,10 @@ sudo mkdir -p /home/vagrant/installer
 # Add Yum repo configuration
 #wget -q -O - https://updates.atomicorp.com/installers/atomic | sudo bash
 
-wget -O /tmp/ossec-hids-3.7.0.tar.gz  http://192.168.1.4/source-code/ossec-hids-3.7.0.tar.gz
+wget -O /tmp/ossec-hids-3.0.0.tar.gz  http://192.168.1.4/source-code/ossec-hids-3.0.0.tar.gz
 cd /tmp     # BEWARE - a directory change !
-# tar xfvz ossec-hids-3.7.0.tar.gz
-# cd ossec-hids-3.7.0
+sudo tar xfvz ossec-hids-3.0.0.tar.gz
+cd ossec-hids-3.0.0
 
 # An expect script is used to perform a basic installation
 sudo cp /tmp/install-ossec-local.exp /home/vagrant/install-ossec-local.exp
@@ -38,5 +39,5 @@ sudo chown vagrant:vagrant /home/vagrant/install-ossec-local.exp
 # sudo systemctl start ossec.service
 # sudo systemctl status ossec.service
 
-echo "Finished setup.sh OK for provisioning OSSEC (local) v2.9.3 on this node"
+echo "Finished setup.sh OK for provisioning OSSEC (local) v3.0.0 on this node"
 echo
