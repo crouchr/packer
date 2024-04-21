1d6d58b824ae5d8d8e7e4bbf9908a05ef59c5813
crouchr <richard.crouch@protonmail.com>
2024-04-21 21:28:37 +0100

    ossec v3.7.0

--- common-packer-scripts/install-ossec-3-local.sh
+++ common-packer-scripts/install-ossec-3-local.sh
@@ -1,15 +1,13 @@
 #!/bin/bash -eux
 # To be customised to Ossec 3
-# FIXME : This is not complete
 
 # Do not run this yet - it prompts for answers
 # https://www.vultr.com/docs/how-to-install-ossec-hids-on-a-centos-7-server/
 # https://linuxadmin.io/ossec-install-centos-7/
 
-# echo 'Installing OSSEC (local) v3.7.0...'
-echo 'Installing OSSEC (local) v3.0.0...'
+echo 'Installing OSSEC (local) v3.7.0...'
 
-sudo yum install -y inotify bind-utils pcre2-devel zlib-devel libevent-devel systemd-devel
+sudo yum install -y inotify bind-utils pcre2-devel zlib-devel libevent-devel systemd-devel openssl-devel
 
 # keep installation files 'tidied away'
 sudo mkdir -p /home/vagrant/installer
@@ -17,27 +15,26 @@ sudo mkdir -p /home/vagrant/installer
 # Add Yum repo configuration
 #wget -q -O - https://updates.atomicorp.com/installers/atomic | sudo bash
 
-wget -O /tmp/ossec-hids-3.0.0.tar.gz  http://192.168.1.4/source-code/ossec-hids-3.0.0.tar.gz
+wget -O /tmp/ossec-hids-3.7.0.tar.gz  http://192.168.1.4/source-code/ossec-hids-3.7.0.tar.gz
 cd /tmp     # BEWARE - a directory change !
-sudo tar xfvz ossec-hids-3.0.0.tar.gz
-cd ossec-hids-3.0.0
+sudo tar xfvz ossec-hids-3.7.0.tar.gz
+cd ossec-hids-3.7.0
 
 # An expect script is used to perform a basic installation
 sudo cp /tmp/install-ossec-local.exp /home/vagrant/install-ossec-local.exp
 sudo chmod +x /home/vagrant/install-ossec-local.exp
 sudo chown vagrant:vagrant /home/vagrant/install-ossec-local.exp
 
-# cd /home/vagrant
-# pwd
-# ls -laF
-# sudo ./install-ossec-local.exp
+export PCRE2_SYSTEM=yes
+sudo ./install-ossec-local.exp
 
 # override the default config file
-# sudo cp /tmp/ossec.conf /var/ossec/etc/ossec.conf
+sudo cp /tmp/ossec.conf /var/ossec/etc/ossec.conf
 
-# sudo systemctl enable ossec.service
-# sudo systemctl start ossec.service
-# sudo systemctl status ossec.service
+sudo systemctl enable ossec.service
+sudo systemctl start ossec.service
+sudo systemctl status ossec.service
 
-echo "Finished setup.sh OK for provisioning OSSEC (local) v3.0.0 on this node"
+echo "Finished setup.sh OK for provisioning OSSEC (local) v3.7.0 on this node"
+
 echo
