457e547c16978e67423119c9c632869eb9d08fc9
crouchr <richard.crouch@protonmail.com>
2024-04-30 14:47:56 +0100

    Replaced OSSEC with Wazuh 3.2.4

---
+++ common-packer-scripts/install-wazuh-local.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -eux
+# See what version of Wazuh can be installed in simplest mode possible
+
+# Do not run this yet - it prompts for answers
+# https://www.vultr.com/docs/how-to-install-ossec-hids-on-a-centos-7-server/
+# https://linuxadmin.io/ossec-install-centos-7/
+
+echo 'Installing Wazuh v3.2.4 (local) ...'
+
+sudo yum install -y inotify bind-utils pcre2-devel zlib-devel libevent-devel systemd-devel openssl-devel
+
+# keep installation files 'tidied away'
+sudo mkdir -p /home/vagrant/installer
+
+# Add Yum repo configuration
+#wget -q -O - https://updates.atomicorp.com/installers/atomic | sudo bash
+
+wget -O /tmp/wazuh-3.2.4.tar.gz  http://192.168.1.4/source-code/wazuh-3.2.4.tar.gz
+cd /tmp     # BEWARE - a directory change !
+sudo tar xfvz wazuh-3.2.4.tar.gz
+cd wazuh-3.2.4
+
+# An expect script is used to perform a basic installation
+sudo cp /tmp/install-wazuh-local.exp /home/vagrant/install-wazuh-local.exp
+sudo chmod +x /home/vagrant/install-wazuh-local.exp
+sudo chown vagrant:vagrant /home/vagrant/install-wazuh-local.exp
+
+cd /home/vagrant
+pwd
+ls -laF
+#export PCRE2_SYSTEM=yes         # not needed in Ossec 2, can also add SQL env var here to build in Mysql support
+sudo ./install-ossec-local.exp
+
+# override the default config file
+sudo cp /tmp/ossec.conf /var/ossec/etc/ossec.conf
+
+# The below is done by the install.sh script
+#sudo systemctl enable ossec.service
+#sudo systemctl start ossec.service
+#sudo systemctl status ossec.service
+
+echo "Finished setup.sh OK for provisioning Wazuh on this node"
+
+echo
