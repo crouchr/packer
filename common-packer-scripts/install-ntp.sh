ae51310a10944ad88ef313caa0ee11ccf6a18607
Richard Crouch <richard.crouch@protonmail.com>
2024-09-26 13:12:37 +0100

    Added NTPD configuration ntp.conf

--- common-packer-scripts/install-ntp.sh
+++ common-packer-scripts/install-ntp.sh
@@ -5,7 +5,9 @@ echo "Installing NTPd..."
 # Need accurate time or Chef knife won't allow bootstrapping
 sudo yum -y install ntp   # yes, its ntp not ntpd
 
+echo "Configuring NTPd..."
+sudo cp /tmp/ntp.conf /etc/ntp/ntp.conf
+
 echo "Starting NTPd..."
 sudo systemctl enable ntpd.service
 sudo systemctl start ntpd.service
-
