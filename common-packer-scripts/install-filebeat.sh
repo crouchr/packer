3453b8a35c18a18c1df65e6f30cfaa9be05af0ab
Richard Crouch <richard.crouch@protonmail.com>
2024-12-04 10:02:36 +0000

    do not install filebeat - temporary hack until i can find issue

--- common-packer-scripts/install-filebeat.sh
+++ common-packer-scripts/install-filebeat.sh
@@ -7,12 +7,15 @@ echo "Installing Elastic Filebeat..."
 wget -O /tmp/filebeat-oss-7.9.2-x86_64.rpm http://192.168.1.4/centos7-packages/filebeat-oss-7.9.2-x86_64.rpm
 sudo yum localinstall -y /tmp/filebeat-oss-7.9.2-x86_64.rpm
 
-sudo cp /tmp/filebeat.yml /etc/filebeat/filebeat.yml
+# fails for some reason so ignore for now
+#sudo cp /tmp/filebeat.yml /etc/filebeat/filebeat.yml
 
 # FIXME : bug means start it during Vagrant provision - not true ?
-echo "Start Filebeat service..."
-sudo /bin/systemctl daemon-reload
-sudo systemctl start filebeat
+#echo "Start Filebeat service..."
+#sudo /bin/systemctl daemon-reload
+#sudo systemctl start filebeat
 
-echo "The following Filebeat modules are available..."
-sudo filebeat modules list
+#echo "The following Filebeat modules are available..."
+#sudo filebeat modules list
+
+echo "Installed Elastic Filebeat."
