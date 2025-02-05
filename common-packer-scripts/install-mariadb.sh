8ba4a6f929407cc51a8a5171c477f86971dd640a
Richard Crouch <richard.crouch@protonmail.com>
2025-02-05 19:25:41 +0000

    Use archived version of MariaDB

--- common-packer-scripts/install-mariadb.sh
+++ common-packer-scripts/install-mariadb.sh
@@ -2,8 +2,8 @@
 # client, server and C connector
 
 echo "Installing MariaDB SQL Database System v10.4 ..."
-
-sudo echo -e "[mariadb]\nname=MariaDB Repository\nbaseurl=http://yum.mariadb.org/10.4/centos7-amd64\ngpgcheck=1\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" | sudo tee /etc/yum.repos.d/MariaDB.repo
+sudo cp /tmp/MariaDB.repo /etc/yum.repos.d/MariaDB.repo
+# sudo echo -e "[mariadb]\nname=MariaDB Repository\nbaseurl=http://yum.mariadb.org/10.4/centos7-amd64\ngpgcheck=1\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" | sudo tee /etc/yum.repos.d/MariaDB.repo
 
 # MariaDB 10.4
 sudo yum -y install MariaDB-server MariaDB-client MariaDB-devel
