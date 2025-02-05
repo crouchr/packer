cb9a3e5624740b857e84cc7502a153c39aae039e
Richard Crouch <richard.crouch@protonmail.com>
2025-02-05 18:17:00 +0000

    Added users : revesting and shorts

--- common-packer-scripts/install-tradr-shell.sh
+++ common-packer-scripts/install-tradr-shell.sh
@@ -9,6 +9,8 @@ sudo chmod +x /usr/bin/tradr-shell
 sudo usermod -s /usr/bin/tradr-shell admin
 sudo usermod -s /usr/bin/tradr-shell johnnybench
 sudo usermod -s /usr/bin/tradr-shell sector
+sudo usermod -s /usr/bin/tradr-shell revesting
+sudo usermod -s /usr/bin/tradr-shell shorts
 sudo usermod -s /usr/bin/tradr-shell bitcoin
 sudo usermod -s /usr/bin/tradr-shell watcher
 sudo usermod -s /usr/bin/tradr-shell tester
