7ee1d3cbc0d5c55ca926e3e83c9619876c2dac80
Richard Crouch <richard.crouch@protonmail.com>
2025-02-08 16:13:55 +0000

    Added sniper user

---
+++ common-packer-scripts/add-user-sniper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eux
+# also need to add user to install-tradr-shell.sh
+# All users have access to this account
+
+echo 'Running add-user-sniper.sh...'
+
+USER=revesting
+sudo adduser ${USER}
+sudo gpasswd -a ${USER} wheel
+sudo mkdir -p /home/${USER}/.ssh
+echo "${USER}:Faguar1968!" | sudo chpasswd
+sudo usermod -aG docker ${USER}
